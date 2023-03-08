679f3e8d3834e91a82314fb555fd93bfcb5ae830
Miguel <miguelaguiar812@gmail.com>
2023-03-07 21:53:50 -0300

    first commit

---
+++ src/first/project/java/PrimeiroProjeto.java
@@ -0,0 +1,15 @@
+package first.project.java;
+
+import first.project.java.model.gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		gato gato = new gato();
+		System.out.println(gato);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author guel1
+ *
+ */
+module first.project.java {
+}
